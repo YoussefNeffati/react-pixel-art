a63fb6323f9d2fcdd643927e0071b8a702286a5e
YoussefNeffati <youssef.neffati06@gmail.com>
2022-11-10 14:45:42 +0100

    init Project

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import '../styles/App.css';
+import React from "react";
+import Editor from "./Editor";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Editor />
+    </div>
+  );
+}
